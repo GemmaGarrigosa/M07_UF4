aad78b6df00447034b2384628381ea35805cc6ae
Gemma <gemma.garrigosa@gmail.com>
2024-03-08 17:22:51 +0100

    tercer commit divendres

--- TIC_BCN_GGF/centre/urls.py
+++ TIC_BCN_GGF/centre/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name='index')
+   path('', views.index, name='index'),
+   path('students/', views.students, name="students"),
+   path('professors/', views.professors, name="professors")
 ]
